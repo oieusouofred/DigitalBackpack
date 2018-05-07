03fc850a01afc26762c8f2aca27e3f7e822acf5e
oieusouofred <oieusouofred@gmail.com>
2018-05-07 13:43:17 -0300

    i3 config and scripts

---
+++ .scripts/lock.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Take a screenshot:
+scrot /tmp/screen.png
+
+# Create a blur on the shot:
+convert /tmp/screen.png -paint 1 -swirl 360 /tmp/screen.png
+
+# Add the lock to the blurred image:
+[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
+
+# Pause music (mocp, mpd and cmus):
+mocp -P
+mpc pause
+cmus-remote -u
+
+# Lock it up!
+i3lock -e -f -c 000000 -i /tmp/screen.png
+
+# If still locked after 20 seconds, turn off screen.
+sleep 20 && pgrep i3lock && xset dpms force off
